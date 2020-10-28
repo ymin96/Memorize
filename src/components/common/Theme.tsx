919bf6cf17887d8f3627b6e74e23ae8898f647ec
Kim YongHwan <ymin96@naver.com>
2020-10-28 17:16:35 +0900

    fix Theme

--- src/components/common/Theme.tsx
+++ src/components/common/Theme.tsx
@@ -1,6 +1,39 @@
+import { Palette } from "@material-ui/core/styles/createPalette";
+import { green } from "@material-ui/core/colors";
+import { Theme, ThemeOptions } from "@material-ui/core/styles/createMuiTheme";
 import { createMuiTheme } from "@material-ui/core/styles";
-import { create } from "domain";
+
+declare module "@material-ui/core/styles/createMuiTheme" {
+    interface Theme {
+        status: {
+            danger: React.CSSProperties["color"];
+        };
+    }
+    interface ThemeOptions {
+        status: {
+            danger: React.CSSProperties["color"];
+        };
+    }
+}
+
+declare module "@material-ui/core/styles/createPalette" {
+    interface Palette {
+        neutral: Palette["primary"];
+    }
+    interface PaletteOptions {
+        neutral: PaletteOptions["primary"];
+    }
+}
 
 const theme = createMuiTheme({
-    
-})
+    status: {
+        danger: "#e53e3e",
+    },
+    palette: {
+        neutral: {
+            main: "#5c6ac4",
+        },
+    },
+});
+
+export default theme;
